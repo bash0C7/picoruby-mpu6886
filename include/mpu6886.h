50924ffd3b3f580cfba87918311ca0562fe219d0
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-04-30 17:52:18 +0900

    chore: remove dummy C extension files (pure-Ruby gem)

--- include/mpu6886.h
+++
@@ -1,8 +0,0 @@
-#ifndef MPU6886_H_
-#define MPU6886_H_
-
-// MPU6886 - Pure Ruby Implementation
-// All functionality implemented in mrblib/mpu6886.rb
-// This header exists for build system compatibility only
-
-#endif // MPU6886_H_
